29a1092a4ecc47fabf09ec840aa36da7b94f10e9
NITISH19112021 <nitishchauhan19999@gmail.com>
2021-11-22 23:24:20 +0530

    add 22-11-2021

--- js/demo.js
+++ js/demo.js
@@ -1,16 +1,16 @@
   // higer order function:
   // 1. map()    2. filter()     3. sort()   4. reduce()
-  // 5.call()    6. bind()       7. apply()
+  // 5.call()    6. bind()       7. apply() find findindex
 
 // higher order function are those function that will take a function as parameter means a callback function.
 // higher order function are the function that accept function as parameter and returns a function.
 
-var a = function sum(b){
+/* var a = function sum(b){
     console.log("hello"+b)
 }
 a(3);
 
-/* function sing(callback){
+ *//* function sing(callback){
     console.log("la alala ");
     callback()
 } */
@@ -336,7 +336,7 @@ stud1.callfun(); */
 //call method takes the argument as string 
 //apply method takes arguments as array
 
-function Student(firstname, lastname, subject){
+/* function Student(firstname, lastname, subject){
     this.firstname = firstname;
     this.lastname = lastname;
     this.subject = subject;
@@ -346,21 +346,406 @@ function Student(firstname, lastname, subject){
 const callfun = function(prefix, suffix){
     console.log(`${prefix} ${this.firstname} ${this.lastname} ${suffix}`);
 }
- const stud1 = new Student('john', 'doe');
+ const stud1 = new Student('john', 'doe'); */
 //callfun.call(stud1, 'mr', 'hello');
 
 
 //callfun.apply(stud1,['mr', 'hello']);
 
 //bind function return a function 
-
+/* 
 var fn = callfun.bind(stud1)
 console.log(fn)
 fn("a","b")
 
+ */
+
+
+/* function testFilter(testArr,obj){
+ let isExist = testArr.filter(i=> i == obj.key);
+if(isExist.length){
+    return 'object exists at position';
+}else{
+    return 'Not exist'
+}
+}
+
+let arr = [{key:'abc', val:5},{key:'bcd', val:15}];
+let testObj = {key:'abc',val:5};
+ console.log(testFilter(arr,testObj));
+// console.log(testMap());
+
+ */
+
+/* function  Studen(name, age){
+    this.name = name;
+    this.age = age;
+    
+}
+
+const fun= function(pref,suf){
+    return `${pref} ${this.name} ${this.age} ${suf}`
+}
+
+var stud = new Studen('johnnn','don')
+console.log(fun.call(stud,'mr',"hel")) */
+//console.log(fun())
+
+//promise, error handling(try catch), sync await, fetch, ajax, higher order function, nodejs architecture
+
+        //promise
+// if promise has two stage 1. resolve 2. reject
+// 1. when promise is resolve then() method is called
+//2. when promise is resolve catch() method is called..
+
+
+//
+/* var disp = document.getElementById('display').innerHTML;
+console.log(disp);
+ */
+/* function f1(){
+var num1 = parseInt(document.getElementById("num1").value);
+var num2 = parseInt(document.getElementById('num2').value);
+var num3=num1+num2;
+console.log("hello"+num3) 
+} */
+//if we do this we get all element and its text which is inside the tag
+//we have a tag <p> and we want to access the value of this then use innerHTML
+//
+/* 
+var obj ={
+    fname : 'nitish',
+    lname: "chauhan"
+}
+
+console.log(obj.fname +" "+obj.lname);
+
+var obj = {
+    fname:'sams',
+    lname:'colvin'
+}
+
+console.log(obj.fname+" "+obj.lname) */
+
+
+//constructor
+//if we have hundrend preson then we to make hundred objects
+
+// so the solution is constructor..
+
+/* 
+function Person(fn,ln){
+    this.fn = fn,
+    this.ln = ln,
+    this.fullname = function(){
+        return this.fn +" "+this.ln
+    }
+}
+
+
+
+obj1 = new Person("nitish","chauhan")
+console.log(obj1.fullname())
+
+obj2 = new Person("mohit", "chauhan")
+console.log(obj2.fn+" " +obj2.ln)
+
+ */
+//prototype : we we make a function constuctor and we want to update the object the use prototype.
+
+// suppose we are working on real projects and 
+
+
+/* function Person(fn,ln){
+    this.fname = fn;
+    this.lname = ln,
+    this.fullname = function(){
+        return this.fname +" "+this.lname;
+    }
+}
+Person.prototype.age = 12;
+var obj = new Person("nitish","chauhan")
+var obj1 =new Person('mohit' ,"chauhan")
+console.log(obj.age)
+console.log(obj1) */
+
+
+//var disp = document.getElementById('display').innerHTML;
+//console.log(document)
+// console.log(document.all)
+//console.log(document.head)
+
+//console.log(disp.siblingchild)
+//var disp = document.getElementsByTagName('p')
+//var disp = document.getElementById('display')
+
+
+//query selector
+
+//var disp = document.querySelector('#display')
+// var disp = document.querySelector('.disp')
+// console.log(disp.children[0])
+
+//create and append element
+
+/* var myul = document.getElementById("disp");
+//console.log(myul)
+var newli = document.createElement('li')
+//console.log(newli)
+let litext = document.createTextNode('bhavesh');
+//console.log(litext)
+newli.appendChild(litext)
+//myul.appendChild(newli) */
+
+/* let myli = document.getElementById('disp');
+let newli = document.createElement('li');
+//console.log(newli);
+let textnode = document.createTextNode('inssi');
+newli.appendChild(textnode)
+myli.appendChild(newli) */
+
+
+//create append and create text node and add text by button
+/* function addElem(){
+ 
+    let myli = document.getElementById('disp');
+    let newli = document.createElement('li');
+    let textnode = document.createTextNode("ram")
+    newli.appendChild(textnode)
+    myli.appendChild(newli)
+    
+} */
+
+//insert li by text box by clicking on button
+/* 
+ function addElem(){
+    
+    let myli = document.getElementById('disp');
+    myli.className = 'new_li'                               // add class name in ul
+    let newli = document.createElement('li');
+    newli.className = 'new_li abc'                            //add classname in li                  
+    let mytext = document.getElementById('disptext').value;
+    let textnode = document.createTextNode(mytext)
+    newli.appendChild(textnode)
+    myli.appendChild(newli)
+    document.getElementById('disptext').value = " "
+}
+ 
+ */
+
+
+//add list element without append method...
+//use textcontent
+/* 
+function addElem(){
+    
+    let myli = document.getElementById('disp');
+    myli.className = 'new_li'                               // add class name in ul
+    let newli = document.createElement('li');
+    // newli.className = 'new_li abc'                            //add classname in li                  
+    newli.textContent = document.getElementById('disptext').value
+    myli.appendChild(newli)
+    document.getElementById('disptext').value = " "
+} */
+ 
+
+//remove element
+
+//document.body.removeChild(document.getElementById('box'));
+
+/* var boxElement = document.getElementById('box');
+// var libox = boxElement.firstElementChild;
+// var libox = boxElement.firstElementChild.nextElementSibling;
+var libox = document.getElementById('li1');  //to remove particular id
+boxElement.removeChild(libox)
+
+ */
 
 
 
 
+/* let box = document.getElementById('box')
+let li = document.createElement('li')
+li.firstElementChild.textContent= 'ramesh';
+box.appendChild(li)
+ */
+/* function f1(){
+
+let box = document.getElementById('box')
+let newli = document.createElement('li')
+
+let name = document.getElementById('name').value;
+let text = document.createTextNode(name);
+
+newli.append(text)
+box.append(newli);
+
+document.getElementById('name').value=" ";
+}
+
+ */
+
+//document.body.removeChild(document.getElementById('box'))
+//event listner:
 
 
+
+
+// by event listner we can perform two or more events(function) on same btn
+
+/* let btn = document.getElementById('btn');
+ btn.addEventListener('click',f1)
+ console.log(btn.getAttribute('class'))
+ btn.addEventListener('click',f2)
+
+function f1(){
+    console.log('first event...');
+    let btn = document.getElementById('btn')
+    btn.removeEventListener('click',f1)
+}
+function f2(){
+    console.log('second event...')
+} */
+
+
+//replace clild
+/* 
+let myul = document.getElementById('disp');
+let pos = myul.firstElementChild;
+// let pos = document.getElementById('first')
+let li = document.createElement('li');
+li.textContent = 'abc'
+myul.replaceChild(li,pos)
+ */
+
+
+//to do list
+
+/*  function addMore(){
+    let name = document.getElementById('name').value;
+    document.getElementById('error').innerHTML = ''
+    if(name==''){
+        document.getElementById('error').innerHTML = "Please Enter the name"
+    }else{
+        let mybox = document.getElementById('box');
+        let myli = document.createElement('li');
+        
+        myli.textContent = name;
+
+        let a = document.createElement("a");
+        a.textContent="x";
+        a.href='javascript:void(0)';
+        a.className ="link"
+        myli.append(a)
+        
+        let pos = mybox.firstElementChild;
+        // console.log(pos)
+        if(pos==null){
+            mybox.append(myli)
+        }else{
+            mybox.insertBefore(myli,pos)
+        }
+
+        // mybox.insertBefore(li,pos)
+        // mybox.append(myli)
+        document.getElementById('name').value = ""
+    }
+}
+
+
+let btn = document.querySelector('ul')
+btn.addEventListener('click', function(e){
+     let box = document.getElementById('box');
+    let myli = e.target.parentNode;
+    box.removeChild(myli)
+})
+ */
+
+/* let btn = document.querySelector('ul');
+// console.log(btn)
+btn.addEventListener('click',function(e){
+    let box = document.getElementById('box');
+    //let myli = e.target;
+     let myli = e.target.parentNode;
+    // console.log(myli)
+    box.removeChild(myli)
+})
+ */
+
+// Date program by javascirpt
+ 
+/* function checkTime(){ 
+    let d = new Date();
+    let day = d.getDay()
+
+        switch(day){
+            case 0:
+                day = "sunday";
+                break;
+            case 1:
+                day="Monday";
+                break
+            case 2:
+                day= "Tuesday"
+                break
+            case 3:
+                day="Wednesday"
+                break
+            case 4:
+                day="Thursday"
+                break
+            case 5:
+                day="Friday";
+                break;
+            case 6:
+                day ="Saturday";
+}
+            let date = d.getDate();
+            let m = d.getMinutes();
+            let s = d.getSeconds();
+             document.getElementById('disp').innerHTML = day+":"+date +":"+ m +":" + s
+             setTimeout(checkTime,1000);  
+}
+checkTime() */ 
+
+//display date 
+
+/* function showclock(){
+    let d= new Date();
+    document.getElementById('disp').innerHTML = d.toLocaleTimeString()      
+}
+setInterval(showclock,1000); */
+
+
+//curd operaton
+
+let id="";
+// localStorage.clear();
+function manageData(){
+    let name = document.getElementById('name').value;
+    if(name==''){
+        document.getElementById('msg').innerHTML = "Please Enter Name";
+
+    }
+    else{
+        var disp = document.getElementById('root');
+        var tr = document.createElement('tr');
+        
+        var td=document.createElement('td')
+        td.textContent = name
+        tr.appendChild(td)
+        disp.appendChild(tr)
+    }
+
+}
+function selectData(){
+
+}
+
+function editData(){
+
+}
+
+function deleteData(){
+
+}
